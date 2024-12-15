5059743c1e3c4c4cc6dc8ad0f29be883b85c62e4
Ariel Campos <ariel.campos94@yahoo.com>
2024-12-15 12:05:54 -0300

    third section

--- src/components/BusinessAdvantages.tsx
+++ src/components/BusinessAdvantages.tsx
@@ -2,6 +2,9 @@ import { v4 as uuidv4 } from "uuid";
 import { BenefitItem } from "../components/BenefitItem";
 import robotImage from "../assets/images/robo.jpg";
 import meetingImage from "../assets/images/meeting.jpg";
+import cad from "../assets/images/cad-worker.jpeg";
+import renderedMachine from "../assets/images/rendered-machine.jpeg";
+import simulationStress from "../assets/images/simulation-stress.jpeg";
 
 export function BusinessAdvantages() {
   return (
@@ -37,11 +40,49 @@ export function BusinessAdvantages() {
 
       <div className="flex flex-col">
         <img className="self-end rounded-3xl w-56" src={robotImage} alt="" />
-        <img className="self-start rounded-3xl w-56 border-4 border-white mt-[-2rem]" src={meetingImage} alt="" />
+        <img
+          className="self-start rounded-3xl w-56 border-4 border-white mt-[-2rem]"
+          src={meetingImage}
+          alt=""
+        />
       </div>
 
-      <div className="flex flex-col">
-        
+      <div className="flex flex-col gap-4">
+        <h5 className="text-center text-white text-xl font-bold text">
+          Transforme Ideias com Modelamento
+        </h5>
+        <p className="text-justify text-white">
+          O modelamento com CAD oferece precisão e inovação, permitindo a
+          visualização e otimização dos projetos antes da execução, garantindo a
+          previsão de erros através de simulações computacionais, resultando em
+          economia de tempo, recursos e acidentes.
+        </p>
+        <h5 className="text-start text-white text-xl font-bold text">
+          Benefícios:
+        </h5>
+        <ul className="flex flex-col gap-4">
+          <li className="text-justify text-white">
+            Desenvolvimento de modelos tridimensionais precisos, permitindo uma
+            visualização realista dos projetos.
+          </li>
+          <li className="text-justify text-white">
+            Avaliação do desempenho estrutural de componentes, identificando
+            pontos críticos e otimizando a segurança.
+          </li>
+          <li className="text-justify text-white">
+            Geração de folhas de processos de fabricação, assegurando
+            comunicação técnica, controle de revisão e padronização.
+          </li>
+        </ul>
+        <div className="flex flex-col mb-4">
+          <img className="self-end rounded-3xl w-56" src={cad} alt="" />
+          <img
+            className="self-start rounded-3xl w-56 border-4 border-white mt-[-2rem] z-10"
+            src={renderedMachine}
+            alt=""
+          />
+          <img className="self-end rounded-3xl w-56 mt-[-2rem]" src={simulationStress} alt="" />
+        </div>
       </div>
     </section>
   );
